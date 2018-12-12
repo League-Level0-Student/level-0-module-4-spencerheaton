d4ed55372e60df71febe1a7f6343de9d5e58fd78
samwerry <league@WTS13.attlocal.net>
2018-12-11 18:26:32 -0800

    yuh

--- sketch_181127a/sketch_181127a.pde
+++ sketch_181127a/sketch_181127a.pde
@@ -11,7 +11,10 @@ fill(250,250,250);
 ellipse(300,420,50,50);
 ellipse(190,430,50,50);
 fill(0,0,0);
-
-ellipse(mouseX,mouseY,10,10);
-ellipse(mouseX,mouseY,10,10);
+if(mouseX<180){mouseX=180;}
+if(mouseX>200){mouseX=200;}
+if (mouseY<400){mouseY=400;}
+if (mouseY>440){mouseY=440;}
+ellipse(mouseX+110,mouseY,10,10);
+ellipse(mouseX,mouseY+10,10,10);
 }
--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -22,9 +22,11 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int a1 = 65;
+		int b1 = 66;
+		int c1 = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -36,24 +38,26 @@ public class FruitQuiz extends KeyAdapter {
 			// 3. If they selected the right fruit, do steps 4 and 7
 			
 				// 4. Call the correct() method
-				
-				// 7. Use the nextQuestion() method to go to question2
+			if(keyCode == c1) { correct();
+			nextQuestion(question2);
+			}
+			// 7. Use the nextQuestion() method to go to question2
 			
 			
 			// 8. else (if they touched something else)
 				
 				// 9. Call the incorrect() method
 		
-		}
+		
 
-	}
+	}}
 
 	private void correct() {
 		// 5. Find a sound for when they get a question right, and drag it into
 		// the 'extra' package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+playSound(bauss.wav);
 
 	}
 
